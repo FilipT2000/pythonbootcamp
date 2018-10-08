f287836fec9a21410c6245d04dcb8e5a9fa943b7
FilipT2000 <f.topolewski@psz.pl>
2018-10-08 20:18:25 +0200

    first

---
+++ dzien1/cwiczenie1.py
@@ -0,0 +1,24 @@
+# To jest komentarz
+"""
+Utwórz zmienne a i b, przypisz wartości liczbowe
+Utwórz zmienna pole prostokąta i przypisz do niej formułę liczaca te pole
+"""
+
+# a = 1
+# b = 3
+# pole_prostokata = a * b
+# print("Pole prostokąta o bokach", str(a)+", "+str(b), " wynosi:", pole_prostokata)
+# print(f"Pole prostokąta o bokach {a}, {b} wynosi: {pole_prostokata}")
+# print('"To jest tekst w cudzysłowiu"')
+# print("\"To jest tekst w cudzysłowiu\"")
+# print("To jest\n wielolinijkowy\n tekst")
+#
+# a = int(input ("Podaj bok a"))
+# b = int(input("Podaj bok b"))
+# pole_prostokata = a * b
+# print(f"Pole prostokąta o bokach {a}, {b} wynosi: {pole_prostokata}")
+
+a = int(input("Podaj długość podstawy trójkąta "))
+b = int(input("Podaj wysokośc trójkąta "))
+pt = int(a*b/2)
+print(f"Pole tego ładnego trójkąta wynosi dokładnie: {pt}")
