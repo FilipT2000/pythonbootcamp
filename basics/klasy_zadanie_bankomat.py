92c5cd8e1b259b5fa9078304f77e137493f25512
FilipT2000 <f.topolewski@psz.pl>
2018-11-28 20:28:33 +0100

    commit 28-11-2018

--- basics/klasy_zadanie_bankomat.py
+++ basics/klasy_zadanie_bankomat.py
@@ -1,4 +1,3 @@
-
 class CashMachine:
 
     def __init__(self):
@@ -21,37 +20,49 @@ class CashMachine:
                 self.banknoty.remove(i)
                 return print(f"Wypłacono {kasa}")
 
-            elif sum(kasa_do_wyplaty) + i<= kasa:
+            elif sum(kasa_do_wyplaty) + i <= kasa:
                 kasa_do_wyplaty.append(i)
                 if sum(kasa_do_wyplaty) == kasa:
                     return print(f"Wypłacono: {kasa_do_wyplaty}")
 
         return usun_banknoty(kasa_do_wyplaty)
 
-
     def usun_banknoty(self):
         self.banknoty.remove(kasa_do_wyplaty)
 
-
-            # elif:
-            #     for i in self.banknoty:
-
-
+        # elif:
+        #     for i in self.banknoty:
 
         pass
 
     def wallet(self):
         pass
 
+
 bankomat = CashMachine()
-bankomat.put_money([50, 100, 100, 200, 20])
-print(bankomat.banknoty)
-bankomat.withdraw(150)
-print(bankomat.banknoty)
-bankomat.withdraw(150)
-print(bankomat.banknoty)
-usun_banknoty()
+
+operacja = input("Podaj typ operacji (W/ Y)")
 
 
+if operacja == 'W':
+    banknoty = input(print("Podaj jakie banknoty wpłacasz. Wpisz je rozdzielając spacja: "))
+    banknoty = banknoty.split()
+    print(banknoty)
+    banknoty = [int(x) for x in banknoty]        #wyrażenie listowe [i**2 for i in range(10)]
+    bankomat.put_money(banknoty)
+
+# wypłata
+if operacja == "Y":
+    kwota_do_wyplaty = int(input("jaką kwotę chcesz wupłacic"))
+    wyplacone = bankomat.withdraw(kwota_do_wyplaty)
+    print(wyplacone)
 
 
+#
+# bankomat.put_money([50, 100, 100, 200, 20])
+# print(bankomat.banknoty)
+# bankomat.withdraw(150)
+# print(bankomat.banknoty)
+# bankomat.withdraw(150)
+# print(bankomat.banknoty)
+# usun_banknoty()
