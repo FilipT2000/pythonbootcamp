746e1089bd8446603319e385d8f75a67ace29c21
FilipT2000 <f.topolewski@psz.pl>
2019-01-07 20:31:10 +0100

    commit 07-01-2019

--- basics/web_apps/biblioteka/books/models.py
+++ basics/web_apps/biblioteka/books/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    imie = models.CharField(max_length=200)
+    nazwisko = models.CharField(max_length=200)
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.Foreignkey(Author, on_delete=models.CASCADE())
     description = models.TextField()
     cover = models.ImageField(upload_to="books_covers/%Y/%m/%d")
 
